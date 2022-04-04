f58c6c25c2636919f49e312c2476bebcdb9d5ee3
ChloeArd <chloe.ardoise@gmail.com>
2022-04-04 14:11:43 +0200

    symfony react

--- react/components/Categories/Categories.jsx
+++ react/components/Categories/Categories.jsx
@@ -1,6 +1,26 @@
 import './Categories.css';
 
-export const Categories = function ({setCategory, categories}) {
+import {useState, useEffect} from 'react';
+
+export const Categories = function ({setCategory}) {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        /**
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', '/api/categories');
+        xhr.responseType = 'json';
+        xhr.onload = () => xhr.status === 200 && setCategories(xhr.response);
+        xhr.send();
+         */
+
+        fetch('/api/categories')
+            .then(response => {
+                if (response.ok) {
+                    response.json()
+                        .then(data => setCategories(data))
+                }
+            })
+    }, []);
 
     return (
         <div className="Categories">
