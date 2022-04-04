322aaf1244cbed81ccfd782885b4f8db0a69f163
ChloeArd <chloe.ardoise@gmail.com>
2022-04-04 16:47:51 +0200

    symfony react

--- src/Controller/API/CartController.php
+++ src/Controller/API/CartController.php
@@ -5,7 +5,6 @@ namespace App\Controller\API;
 use App\Service\SessionCartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
