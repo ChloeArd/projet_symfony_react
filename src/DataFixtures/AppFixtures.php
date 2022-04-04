a25b20463b3c4279537768e6c757a15e8b2d5355
ChloeArd <chloe.ardoise@gmail.com>
2022-04-04 14:12:11 +0200

    symfony react

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        foreach (['software', 'Hardware', 'Autres'] as $categoryName) {
+            $category = (new Category())->setName($categoryName);
+            $manager->persist((object)$category);
+        }
+
+        $manager->flush();
+    }
+}
