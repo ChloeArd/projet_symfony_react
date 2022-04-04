745bb2d658e7a22359b0cebfc5c8c5d01f079774
ChloeArd <chloe.ardoise@gmail.com>
2022-04-04 14:30:19 +0200

    symfony react

--- react/components/Categories/Categories.jsx
+++ react/components/Categories/Categories.jsx
@@ -5,27 +5,38 @@ import {useState, useEffect} from 'react';
 export const Categories = function ({setCategory}) {
     const [categories, setCategories] = useState([]);
     useEffect(() => {
-        /**
+
+        //Avec XHR
         const xhr = new XMLHttpRequest();
         xhr.open('GET', '/api/categories');
         xhr.responseType = 'json';
-        xhr.onload = () => xhr.status === 200 && setCategories(xhr.response);
+        xhr.onload = () => {setCategories(xhr.response)};
         xhr.send();
-         */
 
-        fetch('/api/categories')
+
+        //Fetch et les promesses
+        /**fetch('/api/categories')
             .then(response => {
                 if (response.ok) {
                     response.json()
                         .then(data => setCategories(data))
                 }
             })
+*/
+
+        /**
+        async function getCategories() {
+            const data = await fetch('/api/categories');
+            setCategories(await data.json());
+        }
+        getCategories().catch(() => console.log("Erreur de récupération de catégories !"));
+         */
     }, []);
 
     return (
         <div className="Categories">
             <select onChange={(e) => setCategory(parseInt(e.target.value))}>
-                {categories.map(category => <option value={category.id} key={category.id}>{category.name}</option>)}
+                {categories.map(category => <option value={category.id} key={category.id}>{category.name.charAt(0).toUpperCase() + category.name.slice(1)}</option>)}
             </select>
         </div>
     );
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -10,7 +10,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        foreach (['software', 'Hardware', 'Autres'] as $categoryName) {
+        foreach (['Software', 'Hardware', 'Autres'] as $categoryName) {
             $category = (new Category())->setName($categoryName);
             $manager->persist((object)$category);
         }
